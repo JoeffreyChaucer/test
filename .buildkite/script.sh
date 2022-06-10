aaeec9237c50658652b62ce56b93f667193afdc8
Joeffrey <joeffrey.chaucer.paitan@gmail.com>
2022-06-10 21:24:17 +0800

    update script

--- .buildkite/script.sh
+++ .buildkite/script.sh
@@ -2,18 +2,21 @@
 set -euo pipefail
 
 BaseBranch=$BUILDKITE_PULL_REQUEST_BASE_BRANCH
+PullRequestBranch=$BUILDKITE_BRANCH
+
+echo "$BaseBranch"
+echo "$PullRequestBranch"
 
 git diff --stat "$BaseBranch"
 
-if ! git diff --name-only "$BaseBranch".. | grep -qvE '(.md)'; then
-    echo "Only doc files were updated, not running the CI."
-    exit
+if ! git diff --name-only "$BaseBranch".."$PullRequestBranch" | grep -qvE '(.md)'; then
+  echo "Only doc files were updated, not running the CI."
+  exit
 fi
 
 echo "Running pr-pipeline."
 
-
-buildkite-agent pipeline upload  <<YAML
+buildkite-agent pipeline upload <<YAML
 steps:
   - label: ":building_construction: Quick Build"
     command: "buildkite-agent pipeline upload .buildkite/pr-pipeline.yml"
